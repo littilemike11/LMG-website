c826c5780d538313c47698106e8e9aa314ac93e1
Michael <micaheledquilan@gmail.com>
2024-07-19 17:47:59 -0400

    implemented title screen

--- lmg-website/src/App.jsx
+++ lmg-website/src/App.jsx
@@ -1,23 +1,31 @@
 import { Link } from 'react-router-dom'
 import './App.css'
-
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">LMG</h1>
-      <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-        <li>
-          <Link to="/WebGames">Start Game</Link>
-        </li>
-        <li>
-          <Link to="/Extra">Extra</Link>
-        </li>
-        <li>
-          <Link to="/Credits">Credits</Link>
-        </li>
-      </ul>
+      <div className='relative bgContainer'>
+        <div className="absolute lg:inset-x-0 lg:bottom-0 right-0 bottom-52 w-48 lg:w-full h-16 ...">
+          <ul className=" text-2xl font-bold text-slate-950 gap-x-64 menu menu-vertical lg:menu-horizontal rounded-box">
+            <li className='hover:scale-110'>
+              <Link to="/WebGames">Start Game</Link>
+            </li>
+            <li className='hover:scale-110'>
+              <Link to="/Extra">Extra</Link>
+            </li>
+            <li className='hover:scale-110'>
+              <Link to="/Credits">Credits</Link>
+            </li>
+          </ul>
+        </div>
+        <footer className="footer text-left absolute -bottom-10 left-0 text-xs text-base-content p-3">
+          <aside>
+            <p>Copyright © {new Date().getFullYear()} - All right reserved by LMG Games</p>
+          </aside>
+        </footer>
+      </div>
+
     </>
   )
 }
