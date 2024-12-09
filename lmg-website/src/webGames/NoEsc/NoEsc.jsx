0a9ba204c647e8a889a355156805093135864a86
Michael <michaeledquilan@gmail.com>
2024-12-08 20:19:42 -0500

    imported daisy modal, import images

--- lmg-website/src/webGames/NoEsc/NoEsc.jsx
+++ lmg-website/src/webGames/NoEsc/NoEsc.jsx
@@ -1,10 +1,20 @@
 import LMG from "../../components/game-components/LMG"
+import Modal from "./Modal"
+import asciiArt from "./images/asciiArt.png"
 import "../../game.css"
 export default function NoEsc(){
     return(
         <>
-            <div className="Game">
+            <div className="Game h-[95vh]">
+    
                 <LMG/>
+                <div
+                    className="w-full h-full bg-cover bg-center bg-no-repeat"
+                    style={{backgroundImage:`url(${asciiArt})`}}>
+                    <Modal/>
+                </div>
+                
+                {/* <img src={asciiArt} alt="" /> */}
                 
             </div>
             
