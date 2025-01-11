e2e8abf282057e17e59aba5d1dda6f29c9ce81bc
Michael <michaeledquilan@gmail.com>
2025-01-11 15:21:44 -0500

    implement basic game template

---
+++ lmg-website/src/webGames/NerdBowl/NerdBowl.jsx
@@ -0,0 +1,10 @@
+import LMG from "../../components/game-components/LMG"
+export default function NerdBowl(){
+    return(
+        <>
+            <div className="Game">
+                <LMG />
+            </div>
+        </>
+    )
+}
