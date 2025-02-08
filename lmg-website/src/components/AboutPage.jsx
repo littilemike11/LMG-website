80fb27353b6d259c8a5524520c9968af78d20099
Michael <micaheledquilan@gmail.com>
2025-02-08 16:55:19 -0500

    implemented timeline

--- lmg-website/src/components/AboutPage.jsx
+++ lmg-website/src/components/AboutPage.jsx
@@ -1,98 +1,24 @@
 import NavBar from "./NavBar"
 import Footer from "./Footer"
+import TimelineItem from "./TimelineItem"
+import timeline from "../data/Timeline"
 export default function AboutPage() {
-    return (
-        <>
-
-            <NavBar />
-            <h1 className="text-3xl my-5">Credits</h1>
-            <ul className="timeline timeline-vertical">
-  <li>
-    <div className="timeline-start timeline-box">First Macintosh computer</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
-    </div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
-    </div>
-    <div className="timeline-end timeline-box">iMac</div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start timeline-box">iPod</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
-    </div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
-    </div>
-    <div className="timeline-end timeline-box">iPhone</div>
-    <hr />
-  </li>
-  <li>
-    <hr />
-    <div className="timeline-start timeline-box">Apple Watch</div>
-    <div className="timeline-middle">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="h-5 w-5">
-        <path
-          fillRule="evenodd"
-          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-          clipRule="evenodd" />
-      </svg>
-    </div>
-  </li>
-</ul>
-            <Footer />
-
-        </>
-    )
+  return (
+    <>
+      <NavBar />
+      <h1 className="text-3xl my-5">Credits</h1>
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        {timeline.map((item, index) => (
+          <TimelineItem
+            key={index}
+            time={item.time}
+            title={item.title}
+            description={item.description}
+            left={index % 2 == 0}
+          />
+        ))}
+      </ul>
+      <Footer />
+    </>
+  )
 }
