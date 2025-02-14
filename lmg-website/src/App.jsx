9e231074054a1a7fda7b5986032c0327020037eb
Michael <michaeledquilan@gmail.com>
2025-02-14 18:37:43 -0500

    updated title screen stagger animation

--- lmg-website/src/App.jsx
+++ lmg-website/src/App.jsx
@@ -1,35 +1,48 @@
 import './App.css'
 import Footer from './components/Footer'
 import TitleScreenLinks from './components/TitleScreenLinks'
+import { motion } from 'motion/react'
 function App() {
 
+  const pages = [
+    { title: "Start Game", link: "/WebGames" },
+    { title: "Extra", link: "/Extra" },
+    { title: "Settings", link: "/Settings" },
+    { title: "Credits", link: "/Credits" }
+  ];
 
   return (
     <>
       <div className=' bgContainer'>
         <div className="flex flex-col justify-end items-end w-full h-full ">
-          <ul className="mr-6 mb-12 menu menu-vertical rounded-box">
-            <TitleScreenLinks
-              link={"/WebGames"}
-              title="Start Game"
-              delayTime={0}
-            />
-            <TitleScreenLinks
-              link={"/Extra"}
-              title="Extra"
-              delayTime={.3}
-            />
-            <TitleScreenLinks
-              link={"/Settings"}
-              title="Settings"
-              delayTime={.6}
-            />
-            <TitleScreenLinks
-              link={"/Credits"}
-              title="Credits"
-              delayTime={.9}
-            />
-          </ul>
+          <motion.ul 
+            className="mr-6 mb-12 gap-4 menu menu-vertical rounded-box"
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden:{opacity:0},
+              visible:{opacity:1,
+                transition:{ staggerChildren:.5}
+              }
+            }}
+            >
+            {pages.map((page,index)=>(
+              <motion.li 
+              whileHover={{ scale: 1.1, transition:{duration:.3,ease:"easeOut"} }}
+              key={index}
+              variants={{
+                hidden:{opacity:0, translateX:"100%"},
+                visible:{opacity:1,
+                  translateX: "0%",
+                  transition:{ ease:"easeOut", duration:1}}
+              }}>
+                <TitleScreenLinks
+                  title={page.title}
+                  link={page.link}
+                />
+              </motion.li>
+            ))}
+          </motion.ul>
         </div>
         <div className='absolute bottom-0 left-0'>
           <Footer />
--- lmg-website/src/components/TitleScreenLinks.jsx
+++ lmg-website/src/components/TitleScreenLinks.jsx
@@ -1,20 +1,16 @@
 import { Link } from "react-router-dom"
-import {motion} from "motion/react"
-export default function TitleScreenLinks({ link, title, delayTime }) {
+export default function TitleScreenLinks({ link, title  }) {
     return (
-        <>
-            <motion.div 
-            // whileHover={{scale:"1.1"}}
-                initial={{ transform: "translateX(100%)", opacity:0}}
-                animate={{ transform: "translateX(0%)",opacity:1 }}
-                transition={{ease:"easeOut", duration:1 ,delay:delayTime}}
-                
-                >
-                <Link to={link} >
-                    <button className='hover:scale-110 btn btn-ghost italic tracking-widest w-full h-full font-bold text-3xl p-2 m-2 font-serif text-black shadow-lg border rounded-md' >{title}</button>
+        <>        
+            
+            <button className='flex justify-start btn btn-ghost italic tracking-widest w-full h-full font-bold text-3xl p-2 font-serif text-black shadow-lg rounded-md' >
+                <Link  to={link}>
+                    {title}
                 </Link>
-            </motion.div>
-
+                
+            </button>
+            
+            
         </>
     )
 }
