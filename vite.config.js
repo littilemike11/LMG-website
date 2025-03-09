315e18f7a70a51bd6b38d26e18ff5aa90f5b04d0
Michael <micaheledquilan@gmail.com>
2025-03-09 14:12:20 -0400

    adding redirect

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+// https://vitejs.dev/config/
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: "public/_redirects", dest: "." }],
+    }),
+  ],
+  base: "/", // Keep this since you're using a custom domain
 });
